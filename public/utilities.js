5f4a66aee5957d724f9d3d5ef8c9237938f46af1
David Headrick <davidheadrick1@gmail.com>
2020-03-14 19:13:53 -0400

    debugging

--- public/utilities.js
+++ public/utilities.js
@@ -12,7 +12,7 @@ function loadStates() {
 
     fetch(url)
         .then((response) => {
-            console.log(`response: ${response}`);
+            console.log(response);
             return response.json();
         })
         .then((data) => {
