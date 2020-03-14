fc9ce15d0640100626ca9a4a4cb187da3ce47441
David Headrick <davidheadrick1@gmail.com>
2020-03-14 18:54:39 -0400

    debugging

--- public/utilities.js
+++ public/utilities.js
@@ -2,27 +2,27 @@
 document.querySelector('#getStates').addEventListener('click', loadStates, false);
 
 // let url = 'https://nameless-waters-73580.herokuapp.com/getState?id=1';
-let url = 'https://nameless-waters-73580.herokuapp.com/getState';
+// let url = 'https://nameless-waters-73580.herokuapp.com/getState';
 
-function loadStates(url) {
-    fetch(url)
-        .then((response) => {
-            console.log(response);
-            return response.json();
-        })
-        .then((data) => {
+// function loadStates(url) {
+//     fetch(url)
+//         .then((response) => {
+//             console.log(response);
+//             return response.json();
+//         })
+//         .then((data) => {
 
-            let table = '<table>';
+//             let table = '<table>';
 
-            data.results.forEach(result => {
-                table += '<tr>';
-                table += '<td class="name">' + result.stateName + '</td>';
-                table += '</tr>'
-            })
+//             data.results.forEach(result => {
+//                 table += '<tr>';
+//                 table += '<td class="name">' + result.stateName + '</td>';
+//                 table += '</tr>'
+//             })
 
-            table += '</table>';
+//             table += '</table>';
 
-            document.getElementById('state').innerHTML = table;
+//             document.getElementById('state').innerHTML = table;
 
-        });
-}
+//         });
+// }
