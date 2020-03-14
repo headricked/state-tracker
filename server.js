7f9beb03988458babcaf47d82b01ba3e4de6714b
David Headrick <davidheadrick1@gmail.com>
2020-03-14 18:21:55 -0400

    updated files

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 // add event listener to button
 document.querySelector('#getStates').addEventListener('click', loadStates, false);
 
-function loadStates() {
-    
+let url = 'https://nameless-waters-73580.herokuapp.com/getState?id=1';
+
+function loadStates(url) {
+    fetch(url)
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((data) => {
+
+            let table = '<table>';
+
+            data.results.forEach(result => {
+                table += '<tr>';
+                table += '<td class="name">' + result.stateName + '</td>';
+                table += '</tr>'
+            })
+
+            table += '</table>';
+
+            document.getElementById('state').innerHTML = table;
+
+        });
 }
+
+
+
+
 
 
 
