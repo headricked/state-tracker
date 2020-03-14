b397fcf12364c392ec9161588fc808741c81cdb8
David Headrick <davidheadrick1@gmail.com>
2020-03-14 19:20:47 -0400

    debugging

--- public/utilities.js
+++ public/utilities.js
@@ -3,7 +3,7 @@ document.querySelector('#getStates').addEventListener('click', loadStates, false
 
 console.log('hello');
 
-let url = 'https://nameless-waters-73580.herokuapp.com/getState?id=33';
+let url = 'https://nameless-waters-73580.herokuapp.com/getState?id=35';
 // let joe = 'getState';
 
 function loadStates() {
