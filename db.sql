d89d9cdfc0c7a91fa7686b135d41d43cfe0a0d9c
David Headrick <davidheadrick1@gmail.com>
2020-03-14 14:42:28 -0400

    updated package name

--- db.sql
+++ db.sql
@@ -13,26 +13,3 @@ CREATE TABLE familyTree
     childId INT NOT NULL REFERENCES person(id)
 );
 
-INSERT INTO person (firstName, lastName, dateOfBirth)
-    VALUES ('Mike','Doe','19550125');
-
-INSERT INTO person (firstName, lastName, dateOfBirth)
-    VALUES ('Jill','Doe','19570125');
-
-INSERT INTO person (firstName, lastName, dateOfBirth)
-    VALUES ('Son','Doe','19751125');
-
-INSERT INTO person (firstName, lastName, dateOfBirth)
-    VALUES ('Daughter','Doe','19790320');
-    
-INSERT INTO familyTree (parentId, childId)
-    VALUES ('1','3');
-
-INSERT INTO familyTree (parentId, childId)
-    VALUES ('1','4');
-
-INSERT INTO familyTree (parentId, childId)
-    VALUES ('2','3');
-
-INSERT INTO familyTree (parentId, childId)
-    VALUES ('2','4');
