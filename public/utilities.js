a3af3cc94b0ae603365d67c97e8948106a2a70e4
David Headrick <davidheadrick1@gmail.com>
2020-03-15 00:17:14 -0400

    debugging

--- public/utilities.js
+++ public/utilities.js
@@ -1,22 +1,20 @@
 // add event listener to button
 document.querySelector('#getStates').addEventListener('click', loadStates, false);
 
-let url = 'https://nameless-waters-73580.herokuapp.com/getState?id=30';
+let url = 'https://nameless-waters-73580.herokuapp.com/getState?id=38';
 
 function loadStates() {
     let request = new XMLHttpRequest();
     request.open('GET', url);
     request.responseType = 'json';
-    let state = request.response.statename;
-    document.getElementById("state").innerHTML = state;
 
-    // request.onload = function () {
-    //     console.log(request); // the request
-    //     console.log(request.response); // the response object
-    //     console.log(request.response.statename); // the property of the response object
-    //     let state = request.response.statename;
-    //     document.getElementById("state").innerHTML = state;
-    // };
+    request.onload = function () {
+        console.log(request); // the request
+        console.log(request.response); // the response object
+        console.log(request.response.statename); // the property of the response object
+        let state = request.response.statename;
+        document.getElementById("state").innerHTML = state;
+    };
 
     request.send();
 }
